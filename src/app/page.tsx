04812a06dc51218a1e58ac92ccf7a45e4582b847
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2025-05-07 10:44:20 -0500

    minimum requirements ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid items-center gap-12 justify-items-center p-4 md:p-8 pb-20 sm:p-20 font-[family-name:var(--font-noto-sans)]">
+      <section className="intro-copy-container flex flex-col items-center gap-4">
+        <h1 className="intro-subhead text-indigo-700 text-center font-semibold">One Time Purchase</h1>
+        <h2 className="intro-heading text-center text-3xl md:text-5xl font-semibold">Pay As You Need</h2>
+        <p className="intro-copy text-center max-w-[52ch] text-neutral-600">We offer one-time purchases with credits, for you tu use as needed. Always active.</p>
+      </section>
+      <section className="2-col-pricing-section flex flex-col gap-6 lg:flex-row ">
+        <div className="pricing-list md:px-4 lg:w-1/2">
+          <h2 className="checklist-heading pb-8 text-2xl md:text-5xl font-semibold">Unlock creativity once, enjoy forever</h2>
+          <ul className="flex flex-col gap-4">
+            <li className="intro-copy text-neutral-600 bg-[url(/check-fill.svg)] bg-no-repeat bg-left pl-8">128 available credits for all images</li>
+            <li className="intro-copy text-neutral-600 bg-[url(/check-fill.svg)] bg-no-repeat bg-left pl-8">Up to 3 users</li>
+            <li className="intro-copy text-neutral-600 bg-[url(/check-fill.svg)] bg-no-repeat bg-left pl-8">24 hour response time</li>
+            <li className="intro-copy text-neutral-600 bg-[url(/check-fill.svg)] bg-no-repeat bg-left pl-8">Advanced analytics</li>
+          </ul>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="pricing-card py-8 px-4 border-1 border-neutral-200 shadow-lg rounded-lg lg:w-1/2 relative min-h-[180px] flex flex-col gap-4 items-center">
+          <p className="px-4 py-2 border-2 bg-green-50 border-green-200 text-green-700 rounded-full">Popular</p>
+          <p className="text-center text-5xl md:text-5xl font-semibold">$699</p>
+          <p className="text-neutral-600">Prices in USD</p>
+          <p>Pay once, use it forever.<br /> 
+             No strings attached.</p>
+          <a role="button" href="#"
+          className="bg-indigo-700 text-white w-full text-center rounded-sm py-2 max-w-[90%]">Buy Now</a>
+        </div>
+      </section>
     </div>
   );
 }
